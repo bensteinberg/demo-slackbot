e39612c3a5009a6a4bc57b14060427a2ff29cf8c
Ben Steinberg <bsteinberg@law.harvard.edu>
2016-06-16 13:48:04 -0400

    Cleanup for pep8

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 # a demo aslack bot for deployment to Heroku
+#
+# adapted from
+# https://github.com/textbook/aslack/blob/master/examples/halliwell.py
 
 import logging
 import asyncio
 from aslack.slack_bot import SlackBot
 from collections import OrderedDict
 from textwrap import dedent
+
 
 class SleepyBot(SlackBot):
     """A simple Slack bot
@@ -37,18 +41,16 @@ class SleepyBot(SlackBot):
     async def say_im_awake(self, data):
         """I will say I'm awake"""
         return dict(channel=data['channel'], text="I'm awake!")
-        
+
     MESSAGE_FILTERS = OrderedDict([
         (message_is_for_me, say_im_awake),
     ])
 
-    
 
 if __name__ == "__main__":
-    """Main loop"""
-    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(levelname)s:%(message)s',
+                        level=logging.DEBUG)
     logging.info("I'm awake and logging")
     LOOP = asyncio.get_event_loop()
     BOT = LOOP.run_until_complete(SleepyBot.from_api_token())
     LOOP.run_until_complete(BOT.join_rtm())
-
